734d6195c85532a1570c07b79beb17b9fd39fed6
PolymicroDev <hajdu.dani066@gmail.com>
2022-06-25 16:31:18 +0200

    form files

---
+++ MyFirstForm/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MyFirstForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.encryptor = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.decryptor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // encryptor
+            // 
+            this.encryptor.BackColor = System.Drawing.SystemColors.ControlText;
+            this.encryptor.Cursor = System.Windows.Forms.Cursors.Default;
+            this.encryptor.Font = new System.Drawing.Font("Unispace", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encryptor.ForeColor = System.Drawing.Color.LimeGreen;
+            this.encryptor.Location = new System.Drawing.Point(43, 341);
+            this.encryptor.Margin = new System.Windows.Forms.Padding(6);
+            this.encryptor.Name = "encryptor";
+            this.encryptor.Size = new System.Drawing.Size(192, 56);
+            this.encryptor.TabIndex = 0;
+            this.encryptor.Text = "Encrypt";
+            this.encryptor.UseVisualStyleBackColor = false;
+            this.encryptor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.LawnGreen;
+            this.textBox1.Location = new System.Drawing.Point(248, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.Black;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1.Font = new System.Drawing.Font("Unispace", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.Chartreuse;
+            this.lbl1.Location = new System.Drawing.Point(26, 205);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(196, 27);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Target Folder:";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // decryptor
+            // 
+            this.decryptor.BackColor = System.Drawing.Color.Black;
+            this.decryptor.Font = new System.Drawing.Font("Unispace", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decryptor.ForeColor = System.Drawing.Color.LimeGreen;
+            this.decryptor.Location = new System.Drawing.Point(420, 341);
+            this.decryptor.Margin = new System.Windows.Forms.Padding(6);
+            this.decryptor.Name = "decryptor";
+            this.decryptor.Size = new System.Drawing.Size(192, 56);
+            this.decryptor.TabIndex = 3;
+            this.decryptor.Text = "Decrypt";
+            this.decryptor.UseVisualStyleBackColor = false;
+            this.decryptor.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Unispace", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(117, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ultimate Encrypting Tool";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Unispace", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label2.Location = new System.Drawing.Point(108, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(456, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Use quotation marks for the path! ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.LawnGreen;
+            this.textBox2.Location = new System.Drawing.Point(228, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(408, 31);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Unispace", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label3.Location = new System.Drawing.Point(26, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 27);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tool\'s folder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Unispace", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label4.Location = new System.Drawing.Point(214, 292);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 27);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "e.g: \"D:\\My stuff\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(658, 496);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.decryptor);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.encryptor);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button decryptor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MyFirstForm/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //"/k " + current.Substring(1,2) + "&cd" + current + "move *.py " + path + "&cd "+ path+"&python encryptor1.py"
+            string path;
+            string current;
+            current = textBox2.Text;
+            path = textBox1.Text;
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "CMD.EXE";
+            psi.Arguments = "/c " + current.Substring(1,2) + "&cd " + current + "&move *.py " + path + "&cd "+ path+"&python encryptor.py";
+            p.StartInfo = psi;
+            p.Start();
+            p.WaitForExit();
+
+        }
+
+        private void lbl_msg_Click(object sender, EventArgs e)
+        {
+
+            
+            
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_btn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string path;
+            path = textBox1.Text;
+            Process p = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "CMD.EXE";
+            psi.Arguments = "/c " + path.Substring(1, 2) + "&cd " + path  + "&python decryptor.py";
+            p.StartInfo = psi;
+            p.Start();
+            p.WaitForExit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
