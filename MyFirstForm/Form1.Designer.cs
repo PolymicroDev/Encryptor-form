f5068c5de30d7a57caf52f7afed18d225ce891d8
PolymicroDev <hajdu.dani066@gmail.com>
2022-06-28 14:38:47 +0200

    commitment

--- MyFirstForm/Form1.Designer.cs
+++ MyFirstForm/Form1.Designer.cs
@@ -59,7 +59,7 @@
             // 
             this.textBox1.BackColor = System.Drawing.Color.Black;
             this.textBox1.ForeColor = System.Drawing.Color.LawnGreen;
-            this.textBox1.Location = new System.Drawing.Point(248, 202);
+            this.textBox1.Location = new System.Drawing.Point(238, 201);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(398, 31);
             this.textBox1.TabIndex = 1;
