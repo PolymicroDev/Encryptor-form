6731a95bd8566579c37ea66d9e8d5bb387a166dd
PolymicroDev <hajdu.dani066@gmail.com>
2022-07-01 10:08:57 +0200

    Update

--- MyFirstForm/Form1.Designer.cs
+++ MyFirstForm/Form1.Designer.cs
@@ -175,7 +175,7 @@
             this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ultimate Encrypting Tool";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
